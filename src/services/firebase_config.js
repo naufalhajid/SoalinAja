e99856e3618b23f77839fb0d91820a81f3656e0b
Dontgit <dontgit@gmail.com>
2025-08-26 19:16:32 +0700

    update 2

--- src/services/firebase_config.js
+++ src/services/firebase_config.js
@@ -1,21 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyBzJCONP5jNKG11RZ1epfkdYjYjCXZEB3Q",
+  authDomain: "soalinaja-9299d.firebaseapp.com",
+  projectId: "soalinaja-9299d",
+  storageBucket: "soalinaja-9299d.firebasestorage.app",
+  messagingSenderId: "235638035532",
+  appId: "1:235638035532:web:6631a25f6efd29ef7810e9",
+  measurementId: "G-QGFYC0NWDD"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication and Firestore
-export const auth = getAuth(app);
-export const firestore = getFirestore(app);
-
-export { auth, firestore, app };
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
